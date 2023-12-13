b7aacf45a8c4f5907cab3176fa9582473f703f83
Berk Aksu <berkaksu79@gmail.com>
2023-12-13 12:08:30 +0300

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Berk Aksu on 13.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

a94d777d4fe4e3a71e49cfc686c11a32423856c8
Berk Aksu <berkaksu79@gmail.com>
2023-12-13 18:18:44 +0300

    defining the main text field and loop

--- WordScramble/ContentView.swift
+++ WordScramble/ContentView.swift
@@ -12,8 +12,21 @@ struct ContentView: View {
     @State private var rootWord = ""
     @State private var newWord = ""
     var body: some View {
-
+        NavigationView{
+            List{
+                Section{
+                    TextField("Enter your word", text: $newWord)
+                }
+                Section{
+                    ForEach(usedWords, id: \.self){ word in
+                        Text(word)
+                    }
+                }
+            }
+            .navigationTitle(rootWord)
+        }
     }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
